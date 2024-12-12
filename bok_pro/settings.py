c2e2da8bf8b299803b197f77bd924ad3cb9eac89
Samrawit Tekheste <samrawit.samuel.tk@gmail.com>
2024-12-12 23:11:08 +0000

    added authentication

--- bok_pro/settings.py
+++ bok_pro/settings.py
@@ -43,12 +43,20 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'bok_app',
     'about_app',
     'booking_app',
     'django_summernote', # Summernote integration
     'food_drink',
 ]
+
+SITE_ID = 1
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -59,6 +67,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = 'bok_pro.urls'
@@ -120,6 +129,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
--- bok_pro/views.py
+++ bok_pro/views.py
@@ -3,8 +3,3 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'base.html')
 
-def food(request):
-    return render(request, 'food_menu.html')
-
-def drink(request):
-    return render(request, 'drink_menu.html')
