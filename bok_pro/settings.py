29927a5ca1bdb53afb73ff4ab45439e8e07b341a
Samrawit Tekheste <samrawit.samuel.tk@gmail.com>
2024-12-13 00:35:17 +0000

    Install crispy forms and crispy bootstrap for styling

--- bok_pro/settings.py
+++ bok_pro/settings.py
@@ -52,6 +52,8 @@ INSTALLED_APPS = [
     'booking_app',
     'django_summernote', # Summernote integration
     'food_drink',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
 SITE_ID = 1
@@ -70,6 +72,10 @@ MIDDLEWARE = [
     'allauth.account.middleware.AccountMiddleware',
 ]
 
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+
 ROOT_URLCONF = 'bok_pro.urls'
 
 TEMPLATES = [
@@ -84,6 +90,10 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'builtins':[
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
