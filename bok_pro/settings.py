01b803200d64eaf2fc776fb50e200f4262f6245e
Samrawit Tekheste <samrawit.samuel.tk@gmail.com>
2024-12-19 10:54:52 +0000

    Debug event.html

--- bok_pro/settings.py
+++ bok_pro/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False 
+DEBUG = True
 
 ALLOWED_HOSTS = ["8000-sammy92dec-bokypp4-1jeeh5c5htn.ws.codeinstitute-ide.net", 'boky-pp4-d51abfaa1de4.herokuapp.com']
 
