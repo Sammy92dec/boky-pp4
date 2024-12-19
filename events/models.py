cd83636e7054c8a950eb0abea7cea7e1ca2e28ab
Samrawit Tekheste <samrawit.samuel.tk@gmail.com>
2024-12-19 08:51:33 +0000

    fix:migrate again models from event

--- events/models.py
+++ events/models.py
@@ -17,6 +17,7 @@ class EventComment(models.Model):
     event_post = models.ForeignKey(EventPost, related_name='event_comments', on_delete=models.CASCADE)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at= models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"Comment by {self.author.username} on EventPost {self.event_post.id}"
